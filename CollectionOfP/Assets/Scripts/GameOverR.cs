f4c96342b58581a421ba43c17a084885856e3c45
Yumi Nose <yumi.nose28@gmail.com>
2020-11-20 16:23:38 +0900

    2020.11.20 金曜日 GameOver判定(しようと思ったらできなかった)

---
+++ CollectionOfP/Assets/Scripts/GameOverR.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke("Destroy", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+        Debug.Log("消えた時間" + Time.time);
+    }
+
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("GameOver");
+        }
+    }
+    */
+}
